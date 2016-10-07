5456255abfefe544c69fa1df7622a44b36db3d41
PriyadarshiniJR <priyadarshinijr@gmail.com>
2016-10-07 14:46:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,56 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    myone:{
-           title:'MyFirst Page',
-           content:'This is my first page.'
-    },
-    
-    mytwo:{
-           title:'MySecong Page',
-           content:'This is my second page.'
-    },
-    
-    mythree:{
-           title:'MyThird Page',
-           content:'This is my third page.'
-    }
-};
+var article = {
+	first:{
+		title:'First Shot',
+		h2Content:'I am Priya',
+		wishContent:'How are you?',
+		dateContent:'Today is Friday'
+	},
 
-function template(data){
-    var title=data.title;
-    var content=data.content;
-    var htmlTemplate=`<!DOCTYPE html>
-    <html>
-     <head>
-         <title>
-             ${title}
-         </title>
-         <meta name='viewport' content='width=device=width initial-scale=1'>
-         <link href="/ui/style.css" rel="stylesheet" />
-     </head>   
-     <body>
-         <div class='container'>
-            
-             <div>
-                 <a href='/'>HOME</a>
-             </div>
-             
-             <div>
-                 <hr>
-             </div>
-             
-             <div>
-                 <p>
-                     ${content}
-                </p>
-             </div>
-         </div>
-     </body>
-    </html>
-    `;
-    return htmlTemplate;
+	second:{
+		title :'Second Shot',
+		h2Content:'I am Madhu',
+		wishContent:'Hey all!',
+		dateContent:'Tmrw is Saturday'
+	},
+
+	third:{
+		title :'Third Shot',
+		h2Content:'I am Varun',
+		wishContent:'Sup',
+		dateContent:'Day after is Sunday'
+	}
+}
+
+function createTemp(data){
+	var title =data.title;
+	var h2Content=data.h2Content;
+	var wishContent=data.wishContent;
+	var dateContent=data.dateContent;
+	var htmlTemp = 	`<!DOCTYPE html>
+	<html>
+	<head>
+		<title>
+			$(title)
+		</title>
+		<meta name="viewport" content="width=device-width initial-scale=1">
+		<link href="/ui/style.css" rel="stylesheet" />
+	</head>
+	<body>
+		<div>
+			<h2>
+			$(h2Content)
+			</h2>
+		</div>
+
+		<div>
+			<hr>
+		</div>
+
+		<div>
+			<p>
+				$(wishContent)
+			</p>
+		</div>
+
+		<div>
+			<p>
+				$(dateContent)
+			</p>
+		</div>
+	</body>
+	</html>`;
+	return htmlTemp;
 }
 
 app.get('/', function (req, res) {
@@ -62,8 +74,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:pageName',function(req,res){
-  var pageName = req.params.pageName;
-  res.send(template(articles[pageName]));
+	var pageName=req.params.pageName;
+	res.send(createTemp(article[pageName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
