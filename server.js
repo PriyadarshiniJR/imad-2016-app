999eccea08ccd3af5e7452b1f02f89626a6d3697
PriyadarshiniJR <priyadarshinijr@gmail.com>
2016-10-07 12:51:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/myone',function(req,res){
+ res.send('Article one requested and sent!')    
+});
+
+app.get('/mytwo',function(req,res){
+ res.send('Article two requested and sent!')    
+});
+
+app.get('/mythree',function(req,res){
+ res.send('Article three requested and sent!')    
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
