b71fbf4405731a556992169d7688cab9486c7b5f
PriyadarshiniJR <priyadarshinijr@gmail.com>
2016-10-08 13:32:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,12 @@
 console.log('Loaded!');
 var e = document.getElementById('madi');
-madi.onclick=function(){
-  madi.style.marginLeft='100px';  
-};
+
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
 }
 
-img.onclick=function(){
+madi.onclick=function(){
   var interval=setInterval(moveRight,100);  
 };
