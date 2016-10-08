c83be0ba900cca531326c36207b92cfb308f754c
PriyadarshiniJR <priyadarshinijr@gmail.com>
2016-10-08 13:31:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,12 @@ var e = document.getElementById('madi');
 madi.onclick=function(){
   madi.style.marginLeft='100px';  
 };
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+
+img.onclick=function(){
+  var interval=setInterval(moveRight,100);  
+};
