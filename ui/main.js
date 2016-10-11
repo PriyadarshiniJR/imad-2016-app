fc889dd9fa095126f5367e51d67a15b7cc983448
PriyadarshiniJR <priyadarshinijr@gmail.com>
2016-10-11 13:39:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,11 +25,23 @@ var names = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
-    var nameArray = ['name1','name2','name3'];
-    var list='';
-    for(var i=0;i<nameArray.length;i++){
-        list+='<li>'+nameArray[i]+'</li>';
-    }
-    var ul = document.getElementById('nameList');
-    ul.innerHTML = list; 
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status ===  200){
+                var nameArray = request.responseText;
+                nameArray = JSON.parse(nameArray);
+                var list='';
+                for(var i=0;i<nameArray.length;i++){
+                    list+='<li>'+nameArray[i]+'</li>';
+                }
+                var ul = document.getElementById('nameList');
+                ul.innerHTML = list; 
+          }
+      }
+    };
+    
+    request.open('GET','http://priyadarshinijr.imad.hasura-app.io/submit-name?name='+names,true);
+    request.send(null);
 };
