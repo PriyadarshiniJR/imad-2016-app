3a4e35f624ff0cc93407dd82145fae3e81f504f8
PriyadarshiniJR <priyadarshinijr@gmail.com>
2016-10-11 12:23:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ submit.onclick = function(){
     
     var nameArray = ['name1','name2','name3'];
     var list='';
-    for(var i=0;i<nameArray.length();i++){
+    for(var i=0;i<nameArray.length;i++){
         list+='<li>'+nameArray[i]+'</li>';
     }
     var ul = document.getElementById('nameList');
