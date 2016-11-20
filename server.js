8b313f7c4731a1b419b518b84a06f8309911bfa0
PriyadarshiniJR <priyadarshinijr@gmail.com>
2016-11-20 15:26:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'Index.html'));
 });
 
 app.get('/ui/favicon.ico', function (req, res) {
