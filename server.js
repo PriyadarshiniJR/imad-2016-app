839709c6359953082bff0c17a865a9c5130f6a61
PriyadarshiniJR <priyadarshinijr@gmail.com>
2016-11-24 23:24:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/article.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article.css'));
+});
 
 //JS
 app.get('/ui/main.js', function (req, res) {
