60afab305053f1f5c47043304a15e67bb0ad8344
PriyadarshiniJR <priyadarshinijr@gmail.com>
2016-11-28 09:43:57 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -52,7 +52,7 @@ function loadLogin () {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                //loadCommentForm(request.responseText);
+                loadCommentForm(request.responseText);
             }
         }
     };
