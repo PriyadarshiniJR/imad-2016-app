c031f749d23fdeb18d614907fc1941afcd322b16
PriyadarshiniJR <priyadarshinijr@gmail.com>
2016-12-09 16:22:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,8 @@ app.get('/hash/:input', function(req, res) {
 app.post('/create-user', function (req, res) {
   var username = req.body.username;
   var password = req.body.password;
-  if(!username.trim() || !password.trim() || username.length>32 || password.length>32){
+  var email = req.body.email;
+  if(!username.trim() || !password.trim() || !email.trim() || username.length>32 || password.length>32 || email.length>32){
       res.status(400).send('Cannot leave username or password blank.Please Enter Username/Password:(Upto 32 chars)')
   } 
   else if(!/^[a-zA-Z0-9_.@]+$/.test(username)){  //If username contains other than a-z,A-Z,0-9,@._ then send error.
@@ -154,7 +155,7 @@ app.post('/create-user', function (req, res) {
   else{
         var salt = crypto.randomBytes(128).toString('hex');
         var dbString = hash(password, salt);
-        pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString],         function (err, result) {
+        pool.query('INSERT INTO "user" (username, password,email) VALUES ($1, $2,$3)', [username, dbString,email],         function (err, result) {
            if(err) {
               res.status(500).send(err.toString());
            } else {
