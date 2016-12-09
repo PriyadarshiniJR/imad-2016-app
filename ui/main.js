2567ad4e78446e6dc25cafbaa8f955292c2c1605
PriyadarshiniJR <priyadarshinijr@gmail.com>
2016-12-09 18:44:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -146,7 +146,12 @@ function logout(){
                 alert("Logged out successfully!");
                 window.location.href="http://priyadarshinijr.imad.hasura-app.io/";
             }
-
+            
+            else
+            {
+                //console.log(request.status);
+            alert('Could not logout');
+            }
         }
         
     };
